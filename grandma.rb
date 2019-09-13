3b6157c95b5e0d6426ba9a7098531de1977ca1e0
John Reyes <john.jreye@gmail.com>
2019-09-13 19:47:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 # Write a speak_to_grandma method.
-speak_to_grandma = "I LOVE YOU GRANDMA!".upcase
+speak_to_grandma == "I LOVE YOU GRANDMA!"
 
   if speak_to_grandma == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"

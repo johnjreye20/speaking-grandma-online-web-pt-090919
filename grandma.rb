089c494df679c32d70fdcdddd17808a6656ae33e
John Reyes <john.jreye@gmail.com>
2019-09-13 19:40:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,10 +10,11 @@ speak_to_grandma = "I LOVE YOU GRANDMA!"
   elsif speak_to_grandma == "Hi!"
     puts "HUH?! SPEAK UP, SONNY!"
 
-  elsif speak_to_grandma == "WHAT?"
+  elsif speak_to_grandma == "WHAT DID YOU EAT TODAY?"
     puts "NO, NOT SINCE 1938!"
 
-  else speak_to_grandma == "WHAT DID YOU EAT TODAY?"
+
+  else speak_to_grandma == "WHAT?"
     puts "NO, NOT SINCE 1938!"
 end
 # Whatever you say to grandma, she should respond with
